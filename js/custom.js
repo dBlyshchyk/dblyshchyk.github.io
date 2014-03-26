7fc4f81fb9c152748f865d3b694351b40effe60e
Dzmitry Blyshchyk <dzmitry.blyshchyk@gmail.com>
2014-03-26 04:23:53 +0300

    responsive

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ var lastId,
 function goToPage(pageContainerId) {
     var offsetTop = pageContainerId === "#" ? 0 : $(pageContainerId).offset().top - topMenuHeight;
     $('html, body').stop().animate({
-        scrollTop: Math.round(offsetTop)
+        scrollTop: offsetTop
     }, 800);
 }
 
@@ -92,41 +92,3 @@ $.fn.scrollTo = function(target, options, callback) {
         });
     });
 }
-
-rotate = 1;
-
-function hide_preloader() { //DOM
-    rotate = 0;
-    $("#preloader").fadeOut(1000);
-}
-
-
-
-$(document).ready(function() {
-
-    // calculate height 
-    var screen_ht = $(window).height();
-    var preloader_ht = 10;
-    var padding = (screen_ht / 2) - preloader_ht;
-
-    $("#preloader").css("padding-top", padding + "px");
-
-    // loading animation using script
-
-    function anim() {
-        $("#preloader_image").animate({
-                left: '-1400px'
-            }, 8000,
-            function() {
-                $("#preloader_image").animate({
-                    left: '0px'
-                }, 5000);
-                if (rotate == 1) {
-                    anim();
-                }
-            });
-    }
-
-    anim();
-
-});
